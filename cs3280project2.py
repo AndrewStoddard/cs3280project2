74066103d3848751e4dcafcc25fd2e2ff60835d4
Andrew Stoddard <acs8182000@gmail.com>
2020-09-28 13:35:15 -0400

    initial commit

---
+++ cs3280project2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+'''
+Project 2
+'''
+import sys
+
+__author__ = "AndrewStoddard"
+__version__ = "Fall 2020"
+
+def main(arg1):
+    '''
+    The main function
+    '''
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
