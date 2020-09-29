1df916f82a097e64c988622c2e50fc4b58a9b7f5
Andrew Stoddard <acs8182000@gmail.com>
2020-09-29 17:02:19 -0400

    Split up utilities and main and refactored validation

--- cs3280project2.py
+++ cs3280project2.py
@@ -4,118 +4,52 @@
 Project 2
 '''
 import sys
-import re
+import utils
 
 __author__ = "AndrewStoddard"
 __version__ = "Fall 2020"
 
-subnet_maxed_octets = 0
-subnet_active_bits = 0
 
-def isValidOctet(octet):
-    regex = re.compile(r"\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b")
-    if regex.match(octet):
-        return True
-    return False
 
-def find_maxed_octets(subnet_mask):
-    if (not isValidOctet(subnet_mask)):
-        try:
-            if (int(subnet_mask) < 32 and int(subnet_mask) > 0):
-                return int(subnet_mask) // 8
-            else:
-                print("Invalid Subnet Mask")
-                return
-        except:
-            print("Invalid Subnet Mask")
-            return
-    else: 
-        return len(re.findall(re.compile(r"255"), subnet_mask))
 
-def find_active_bits(subnet_mask):
-    if (not isValidOctet(subnet_mask)):
-        try:
-            if (int(subnet_mask) < 32 and int(subnet_mask) > 0):
-                return int(subnet_mask) % 8
-            else:
-                print("Invalid Subnet Mask")
-                return
-        except:
-            print("Invalid Subnet Mask")
-            return
-    else: 
-        octets = subnet_mask.split('.')
-        active_octet = ''
-        for octet in octets:
-            if (not octet == '255' and not octet == '0'):
-                active_octet = octet
-        if active_octet == '':
-            active_octet = '0'
-        active_binary = bin(int(active_octet))
-        return count_active_bits(active_binary)
 
-def count_active_bits(binary):
-    binary = binary.replace('0b', '')
-    count = 0
-    for bit in binary:
-        if bit == '1':
-            count += count + 1
-    return count
 
-        
-def find_subnet_number(bits):
-    subnet_number = ''
-    for i in range(0, subnet_maxed_octets):
-        subnet_number += '255.'
-    binary = get_binary_for_8_bits(subnet_active_bits)
-    value = convert_binary_to_dec(binary)
-    subnet_number += str(value)
-    inactive_intervals = 4 - subnet_maxed_octets
-    for i in range(1, inactive_intervals):
-        if (i < inactive_intervals):
-            subnet_number += '.0'
 
-    return subnet_number
 
-def convert_binary_string_to_list(binary):
-    binary = binary.replace('0b', '')
-    print(binary)
-    binary_list = []
-    for letter in binary:
-        binary_list.append(letter)
-    return binary_list
 
-def get_binary_for_8_bits(number_of_active_bits):
-    binary = "0b"
-    for i in range(0, 8):
-        if (i < number_of_active_bits):
-            binary += '1'
-        else:
-            binary += '0'
-    return binary
 
-def convert_binary_to_dec(binary):
-    binary_list = convert_binary_string_to_list(binary)
-    value = 0
-    for i in range(len(binary_list)):
-        digit = binary_list.pop()
-        if (digit == '1'):
-            value = value + pow(2, i)
-    return value
 
 def main(arg1, arg2):
     '''
     The main function
     '''
-    subnet_active_bits = find_active_bits(arg2)
-    subnet_maxed_octets = find_maxed_octets(arg2)
-    octets = arg1.split('.')
+    is_subnet_mask_in_bits = False
+    if (not utils.is_valid_ip(arg1)):
+        print("Invalid IP")
+
+    if (not utils.is_valid_subnet_mask(arg2)):
+        try:
+            if (int(arg2) < 32 and int(arg2) > 0):
+                is_subnet_mask_in_bits = True
+            else:
+                print("Invalid Subnet Mask")
+                return
+        except:
+            print("Invalid Subnet Mask")
+            return
+            
+    ip = arg1
+    subnet_mask = arg2
+    octets = ip.split('.')
+
+    subnet_active_bits = utils.find_active_bits(subnet_mask, is_subnet_mask_in_bits)
+    subnet_maxed_octets = utils.find_maxed_octets(subnet_mask, is_subnet_mask_in_bits)
+   
     
-    binary_of_active = get_binary_for_8_bits(subnet_active_bits)
+    binary_of_active = utils.to_binary_from_active_bits(subnet_active_bits)
     binary_of_changing_octet = bin(int(octets[subnet_maxed_octets]))[2:].zfill(8)
-    print("binco" + binary_of_changing_octet)
-    binary_list_of_active = convert_binary_string_to_list(binary_of_active)
-    binary_list_of_changing_octet = convert_binary_string_to_list(binary_of_changing_octet)
+    binary_list_of_active = utils.convert_binary_string_to_list(binary_of_active)
+    binary_list_of_changing_octet = utils.convert_binary_string_to_list(binary_of_changing_octet)
     
     new_binary_for_changing = '0b'
     for i in range(0, 8):
@@ -123,7 +57,7 @@ def main(arg1, arg2):
             new_binary_for_changing += '1'
         else:
             new_binary_for_changing += '0'
-    changed_octet = convert_binary_to_dec(new_binary_for_changing)
+    changed_octet = utils.convert_binary_to_dec(new_binary_for_changing)
 
     new_ip = ''
     for octet_index in range(0, len(octets)):
@@ -134,6 +68,7 @@ def main(arg1, arg2):
         else:
             new_ip += '.0'
     print(new_ip)
+
 
 if __name__ == "__main__":
     main(sys.argv[1], sys.argv[2])
---
+++ utils.py
@@ -0,0 +1,82 @@
+import re
+
+__author__ = "AndrewStoddard"
+__version__ = "Fall 2020"
+
+
+def convert_binary_to_dec(binary):
+    binary_list = convert_binary_string_to_list(binary)
+    value = 0
+    for i in range(len(binary_list)):
+        digit = binary_list.pop()
+        if (digit == '1'):
+            value = value + pow(2, i)
+    return value
+
+def convert_binary_string_to_list(binary):
+    binary = binary.replace('0b', '')
+    binary_list = []
+    for letter in binary:
+        binary_list.append(letter)
+    return binary_list
+
+def to_binary_from_active_bits(number_of_active_bits):
+    binary = "0b"
+    for i in range(0, 8):
+        if (i < number_of_active_bits):
+            binary += '1'
+        else:
+            binary += '0'
+    return binary
+
+def is_valid_ip(ip):
+    regex = re.compile(r"\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b")
+    if regex.match(ip):
+        return True
+    return False
+
+def is_valid_subnet_mask(subnet_mask):
+    octets = ['0', '128', '192', '224', '240', '248', '252', '254']
+
+    subnet_mask_octets = subnet_mask.split(".")
+    has_to_be_0 = False
+    for octect_index in range(0, len(subnet_mask_octets)):
+        
+        if (subnet_mask_octets[octect_index] == '255' and not has_to_be_0):
+            pass
+        elif (subnet_mask_octets[octect_index] in octets and not has_to_be_0):
+            has_to_be_0 = True
+        elif (not subnet_mask_octets[octect_index] == '0' and has_to_be_0):
+            return False
+    return True
+
+def count_active_bits(binary):
+    binary = binary.replace('0b', '')
+    count = 0
+    for bit in binary:
+        if bit == '1':
+            count += count + 1
+    return count
+
+def find_maxed_octets(subnet_mask, bit_form):
+    if (bit_form):
+        return int(subnet_mask) // 8
+    else: 
+        return len(re.findall(re.compile(r"255"), subnet_mask))
+
+def find_active_bits(subnet_mask, bit_form):
+    if (bit_form):
+        return int(subnet_mask) % 8
+    else: 
+        subnet_mask_octets = subnet_mask.split('.')
+        active_octet = ''
+        last_octet = ''
+        for octet in subnet_mask_octets:
+            if (not octet == '255' and not octet == '0'):
+                active_octet = octet
+            last_octet = octet
+        if active_octet == '':
+            active_octet = last_octet
+        active_binary = bin(int(active_octet))
+        return count_active_bits(active_binary)
+
