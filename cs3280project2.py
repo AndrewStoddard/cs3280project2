9eb08ea46c9fcea3f1d5c352babc3f6703995513
Andrew Stoddard <acs8182000@gmail.com>
2020-09-29 13:39:51 -0400

    working iteration of getting subnet?

--- cs3280project2.py
+++ cs3280project2.py
@@ -49,6 +49,8 @@ def find_active_bits(subnet_mask):
         for octet in octets:
             if (not octet == '255' and not octet == '0'):
                 active_octet = octet
+        if active_octet == '':
+            active_octet = '0'
         active_binary = bin(int(active_octet))
         return count_active_bits(active_binary)
 
@@ -76,7 +78,8 @@ def find_subnet_number(bits):
     return subnet_number
 
 def convert_binary_string_to_list(binary):
-    binary.replace('0b', '')
+    binary = binary.replace('0b', '')
+    print(binary)
     binary_list = []
     for letter in binary:
         binary_list.append(letter)
@@ -106,8 +109,31 @@ def main(arg1, arg2):
     '''
     subnet_active_bits = find_active_bits(arg2)
     subnet_maxed_octets = find_maxed_octets(arg2)
+    octets = arg1.split('.')
     
+    binary_of_active = get_binary_for_8_bits(subnet_active_bits)
+    binary_of_changing_octet = bin(int(octets[subnet_maxed_octets]))[2:].zfill(8)
+    print("binco" + binary_of_changing_octet)
+    binary_list_of_active = convert_binary_string_to_list(binary_of_active)
+    binary_list_of_changing_octet = convert_binary_string_to_list(binary_of_changing_octet)
+    
+    new_binary_for_changing = '0b'
+    for i in range(0, 8):
+        if (int(binary_list_of_active[i]) & int(binary_list_of_changing_octet[i])):
+            new_binary_for_changing += '1'
+        else:
+            new_binary_for_changing += '0'
+    changed_octet = convert_binary_to_dec(new_binary_for_changing)
 
+    new_ip = ''
+    for octet_index in range(0, len(octets)):
+        if (octet_index < subnet_maxed_octets):
+            new_ip += octets[octet_index] + '.'
+        elif (octet_index == subnet_maxed_octets):
+            new_ip += str(changed_octet)
+        else:
+            new_ip += '.0'
+    print(new_ip)
 
 if __name__ == "__main__":
     main(sys.argv[1], sys.argv[2])
